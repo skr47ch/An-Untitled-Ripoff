28d68fda505eaf1bfad0883b8f160d800cd79d9b
skr47ch_g4m3z <its.me.arrived@gmail.com>
2016-05-21 01:16:07 +0530

    Player can now jump
    Player is able to jump but has infinite jumps.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public Transform thisTransform;
+	public Rigidbody2D thisRigidbody;
+
+	public float jumpForce = 100;
+	public bool isGrounded = true;
+
 	// Use this for initialization
 	void Start () {
-	
+		thisRigidbody = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButton("Jump")){
+			thisRigidbody.AddForce(new Vector2(0, jumpForce));
+			isGrounded = false;
+		}
 	}
+		
 }
