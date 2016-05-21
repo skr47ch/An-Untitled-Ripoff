2e34dc7a690a3ffe561eac97c25e344b2d67c2dd
skr47ch_g4m3z <its.me.arrived@gmail.com>
2016-05-21 20:44:26 +0530

    Still basic movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+	public Camera cameraFollow;
+	public float xOffset = 0.5f;
+	public float yOffset = 0.1f;
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (target)
+		{
+			Vector3 point = cameraFollow.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - cameraFollow.ViewportToWorldPoint(new Vector3(xOffset, yOffset, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
 	}
 }
