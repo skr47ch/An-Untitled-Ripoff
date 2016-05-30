531162fd16f9b7e971476b52b61c01d58fde853e
skr47ch_g4m3z <its.me.arrived@gmail.com>
2016-05-31 02:07:11 +0530

    Basic Player Control Complete using Raycast2D
    Copied SebLague's script. :( too hard.

--- Assets/PlayerController_RayCast.cs
+++ Assets/PlayerController_RayCast.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class PlayerController_RayCast : MonoBehaviour {
 
 	float gravity = 1f;
-	float maxSpeed = 5;
-	float maxFall = 2;
-	float jump = 2;
-	float acceleration = 1f;
+	float maxSpeed = 10f;
+	float maxFall = 15f;
+	float jump = 8f;
+	float acceleration = 2f;
 
-	int layerMask;
+	public LayerMask layerMask;
 	Rect box;
 	Vector2 velocity;
 	bool grounded = false;
@@ -17,16 +17,19 @@ public class PlayerController_RayCast : MonoBehaviour {
 
 	int horizontalRays = 6;
 	int verticalRays = 4;
-	float margin = 0.2f;
+	int margin = 1;
+
+	bool connected = false;
 
 	BoxCollider2D collide;
 	// Use this for initialization
 	void Start () {
-		layerMask = LayerMask.NameToLayer("CollisionLayer");
+//		layerMask = LayerMask.NameToLayer("CollisionLayer");
 		collide = GetComponent<BoxCollider2D>();
 	}
 
 	void FixedUpdate () {
+
 		box = new Rect(
 			collide.bounds.min.x,
 			collide.bounds.min.y,
@@ -41,64 +44,61 @@ public class PlayerController_RayCast : MonoBehaviour {
 		if(velocity.y < 0) falling = true;
 
 		if(grounded || falling) {
-			Debug.Log("falling =" + falling);
 
 			Vector2 startPoint = new Vector2(box.xMin + margin, box.center.y);
 			Vector2 endPoint = new Vector2(box.xMax - margin, box.center.y);
 
-			RaycastHit2D hit;
+			RaycastHit2D hitInfo;
 
 			// add half box height since we're starting in centre
 			float distance = box.height/2 + (grounded ? margin : Mathf.Abs(velocity.y * Time.deltaTime));
-
 			// check if we hit anything
-			bool connected = false;
 
-			for (int i = 0; i<verticalRays; i++) {
-				
-				float lerpAmount = (float)i / (float)verticalRays - 1;
+
+			for (int i = 0; i < verticalRays; i++) {
+				float lerpAmount = (float)i / (float) (verticalRays - 1);
 				Vector2 origin = Vector2.Lerp(startPoint, endPoint, lerpAmount);
 
-				Ray2D ray = new Ray2D(origin, Vector2.down);
-				Debug.DrawRay( origin, Vector3.down, Color.green );
-				hit = Physics2D.Raycast(ray.origin, ray.direction, distance, layerMask);
+				hitInfo = Physics2D.Raycast(origin, Vector2.down, distance, layerMask);
+				Debug.DrawRay(origin, Vector2.down);
 
-//				Debug.Log(connected);
-
-				if(hit) {
-					grounded = true;
-					falling = false;
-					Debug.Log("grounded =" + grounded);
-					transform.Translate(Vector2.down * (distance - box.height/2));
-					velocity = new Vector2(velocity.x, 0);
-					break;
+				if(hitInfo.fraction > 0) {
+					connected = true;
 				}
 			}
-//
-//			if(!connected) {
-//				grounded = false;
-//			}
+			if(connected) {
+				grounded = true;
+				falling = false;
+				transform.Translate(Vector2.down * (distance - box.height/2));
+				velocity = new Vector2(velocity.x, 0);
+				Debug.Log("grounded4 =" + grounded);
+			}
+			else {
+				grounded = false;
+			}
 		}
+
 
 
 		// ---------------------------------//
 		// ---- Lateral Movement -----------//
 		// --------------------------------=//
-
-		float horizontalAxis = Input.GetAxisRaw("Horizontal");
-		float newVelocityX = velocity.x;
-
-		if(horizontalAxis != 0) {
-			newVelocityX  += acceleration * horizontalAxis;
-			newVelocityX = Mathf.Clamp(newVelocityX, -maxSpeed, maxSpeed);
-		}
-		else if(velocity.x != 0) {
-			int modifier = velocity.x > 0 ? -1 : 1;
-			newVelocityX += acceleration * modifier;
-		}
+//
+//		float horizontalAxis = Input.GetAxisRaw("Horizontal");
+//		float newVelocityX = velocity.x;
+//
+//		if(horizontalAxis != 0) {
+//			newVelocityX  += acceleration * horizontalAxis;
+//			newVelocityX = Mathf.Clamp(newVelocityX, -maxSpeed, maxSpeed);
+//		}
+//		else if(velocity.x != 0) {
+//			int modifier = velocity.x > 0 ? -1 : 1;
+//			newVelocityX += acceleration * modifier;
+//		}
 	}
 
 	void LateUpdate () {
-		transform.Translate(velocity * Time.deltaTime);
+		transform.Translate(velocity * Time.deltaTime * 0.1f);
+//		Debug.Log(velocity);
 	}
 }
